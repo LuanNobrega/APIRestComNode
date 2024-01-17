2e38084ae25c7eac438555003509fbec1da94300
LuanNobrega <luan.snobrega@gmail.com>
2024-01-17 10:45:25 -0300

    finalizado

--- routes.js
+++ routes.js
@@ -19,17 +19,18 @@ const orcamentoController = new OrcamentoController();
 
 //Método interceptador, sempre vai cair aqui primeiro. Intercepta todas as rotas e aqui podemso tomar qualquer ação antes de bater em uma rota específica. 
 routes.use(async (req, res, next) => {
+
     const { authorization } = req.headers;
     let autenticado = await usuarioService.validarAutenticacao(authorization);
-
-    //Para entrar o usuário não pode está autenticado e nem pode estar na url de login.
-    if(!autenticado && req.originalUrl != '/login'){
+    
+    if(!autenticado && req.originalUrl !== '/login' && process.env.AUTENTICACAO != 'FALSE'){
         return res.status(401).json({
             status: 401,
-            message: 'Usuário não autorizado',
+            message:'Usuário não autenticado',
             name: 'NaoAutorizado'
-        })
+        });
     }
+
     next();
 })
 
